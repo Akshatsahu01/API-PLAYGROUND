252cdad8237725f10eb3d37501c1c0919c4a4d5c
Akshat <akshat.sahu.s.125@kalvium.community>
2026-06-28 23:27:39 +0530

    chores:modified folder structure

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar/Navbar.jsx";
+// import Hero from "../components/Hero/Hero";
+// import ApiExplorer from "../components/ApiExplorer/ApiExplorer";
+// import CodeGenerator from "../components/CodeGenerator/CodeGenerator";
+// import Footer from "../components/Footer/Footer";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      {/* <Hero />
+      <ApiExplorer />
+      <CodeGenerator />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default Home;
