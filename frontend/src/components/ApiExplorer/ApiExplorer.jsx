6681155a25777db9c77c4ab835393e04438dd45d
Akshat <akshat.sahu.s.125@kalvium.community>
2026-07-02 20:41:32 +0530

    built  filter saving function and applied it

--- frontend/src/components/ApiExplorer/ApiExplorer.jsx
+++ frontend/src/components/ApiExplorer/ApiExplorer.jsx
@@ -6,6 +6,15 @@ import { useState } from "react";
 function ApiExplorer() {
   const [selectedApi, setSelectedApi] = useState("doctors");
   const currentApi = apiConfig[selectedApi];
+  const [selectedFilters, setSelectedFilters] = useState({});
+  function handleFilterChange(event) {
+    const { id, value } = event.target;
+
+    setSelectedFilters((previousFilters) => ({
+      ...previousFilters,
+      [id]: value,  //without square breaket id will not be treated as a veriable
+    }));
+  }
   return (
     <section className="api-explorer">
       <div className="api-container">
@@ -26,52 +35,49 @@ function ApiExplorer() {
         <div className="filter-panel">
           <div className="filter-header">
             <h3>Filters to apply on URL</h3>
-                <div className="filter-buttons">
+            <div className="filter-buttons">
+              <button className="reset-btn">Reset Filters</button>
 
-      <button className="reset-btn">
-        Reset Filters
-      </button>
-
-      <button className="apply-btn">
-        Apply Filters
-      </button>
-
-    </div>
+              <button className="apply-btn">Apply Filters</button>
+            </div>
           </div>
-           <div className="filter-grid">
+          <div className="filter-grid">
+            {currentApi.filters.map((filter) => (
+              <div className="filter-item" key={filter.id}>
+                <label htmlFor={filter.id}>{filter.label}</label>
 
-            
-          {currentApi.filters.map((filter) => (
-            <div className="filter-item" key={filter.id}>
-              <label htmlFor={filter.id}>{filter.label}</label>
+                <select
+                  id={filter.id}
+                  value={selectedFilters[filter.id] || ""}
+                  onChange={handleFilterChange}
+                >
+                  <option value="">Select {filter.label}</option>
 
-              <select id={filter.id}>
-                <option>Select {filter.label}</option>
-
-                {filter.options.map((option) => (
-                  <option key={option}>{option}</option>
-                ))}
-              </select>
-            </div>
-          ))}
-           </div>
+                  {filter.options.map((option) => (
+                    <option key={option}>{option}</option>
+                  ))}
+                </select>
+              </div>
+            ))}
+          </div>
         </div>
 
-       {/* URL Section */}
+        {/* URL Section */}
         <div className="url-section">
-            <label >Final URL</label>
-            <div className="url-row">
-                <input type="text" value={`https://api-playground.com${currentApi.endpoint}`} readOnly />
-                <button>fetch data</button>
-            </div>
- 
+          <label>Final URL</label>
+          <div className="url-row">
+            <input
+              type="text"
+              value={`https://api-playground.com${currentApi.endpoint}`}
+              readOnly
+            />
+            <button>fetch data</button>
+          </div>
         </div>
-           {/* Response Section */}
-           <ResponseViewer/>
+        {/* Response Section */}
+        <ResponseViewer />
       </div>
-
-
-
+      {console.log(selectedFilters)}
     </section>
   );
 }
