463e9069455a86730dde664c32ba9c85cfa6d99c
Akshat <akshat.sahu.s.125@kalvium.community>
2026-09-01 13:47:32 +0530

    feat:Created Api-response component

--- frontend/src/components/ApiExplorer/ApiExplorer.jsx
+++ frontend/src/components/ApiExplorer/ApiExplorer.jsx
@@ -3,25 +3,43 @@ import ResponseViewer from "./ResponseViewer";
 import apiConfig from "../../data/apiConfig";
 import Codegenerator from "../CodeGenerator/CodeGenerator";
 import { useState } from "react";
+import Apimetadata from "./ApiMetadata";
 
 function ApiExplorer() {
   const [selectedApi, setSelectedApi] = useState("doctors");
   const currentApi = apiConfig[selectedApi];
   const [selectedFilters, setSelectedFilters] = useState({});
   const [generatedUrl, setGeneratedUrl] = useState(
-  `${import.meta.env.VITE_API_URL}/api${currentApi.endpoint}`
-)
+    `${import.meta.env.VITE_API_URL}/api${currentApi.endpoint}`,
+  );
   const [responseData, setResponseData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [apiMetadata, setApiMetadata] = useState({});
   async function handleFetchData() {
+    const startTime = Date.now();
     try {
       setLoading(true);
       setError("");
+      const response = await fetch(generatedUrl, { method: "GET" });
+      const endTime = Date.now();
+      const duration = Math.round(endTime - startTime);
 
-      const response = await fetch(generatedUrl);
-
+      setApiMetadata({
+        method: "GET",
+        status: response.status,
+        statusText: response.statusText,
+        responseTime: duration,
+        contentType: response.headers.get("content-type"),
+      });
       if (!response.ok) {
+        setApiMetadata({
+          method: "GET",
+          status: response.status,
+          statusText: response.statusText,
+          responseTime: duration,
+          contentType: response.headers.get("content-type"),
+        });
         throw new Error("Failed to fetch data");
       }
 
@@ -29,128 +47,142 @@ function ApiExplorer() {
 
       setResponseData(data);
 
-      console.log("data fetched : ",data);
-
+      console.log("data fetched : ", data);
     } catch (err) {
+      const endTime = Date.now();
+      const duration = Math.round(endTime - startTime);
+      setApiMetadata({
+        method: "GET",
+        status: "Network Error",
+        statusText: "Unable to reach server",
+        responseTime: duration,
+        contentType: "N/A",
+      });
+
       setError(err.message);
     } finally {
       setLoading(false);
     }
-}
+  }
   function handleFilterChange(event) {
     const { id, value } = event.target;
 
     setSelectedFilters((previousFilters) => {
-        if(value===""){
-            const updatedFilters={...previousFilters}
-            delete updatedFilters[id]
-            return updatedFilters
-        }
-        return {
-
-            ...previousFilters,
-            [id]: value,  //without square breaket id will not be treated as a veriable
-        }
+      if (value === "") {
+        const updatedFilters = { ...previousFilters };
+        delete updatedFilters[id];
+        return updatedFilters;
+      }
+      return {
+        ...previousFilters,
+        [id]: value, //without square breaket id will not be treated as a veriable
+      };
     });
   }
   function handleApplyFilters() {
+    const queryParams = Object.entries(selectedFilters)
+      .filter(([key, value]) => value !== "")
+      .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+      .join("&");
 
-  const queryParams = Object.entries(selectedFilters)
-    .filter(([key, value]) => value !== "")
-    .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-    .join("&");
+    const url =
+      queryParams.length > 0
+        ? `${import.meta.env.VITE_API_URL}/api${currentApi.endpoint}?${queryParams}`
+        : `${import.meta.env.VITE_API_URL}/api${currentApi.endpoint}`;
 
-  const url =
-    queryParams.length > 0
-      ? `${import.meta.env.VITE_API_URL}/api${currentApi.endpoint}?${queryParams}`
-      : `${import.meta.env.VITE_API_URL}/api${currentApi.endpoint}`;
+    setGeneratedUrl(url);
+  }
 
-  setGeneratedUrl(url);
-}
+  function handleResetFilters() {
+    setSelectedFilters({});
 
-function handleResetFilters() {
-  setSelectedFilters({});
-
-  setGeneratedUrl(
-    `${import.meta.env.VITE_API_URL}/api${currentApi.endpoint}`
-  );
-}
+    setGeneratedUrl(
+      `${import.meta.env.VITE_API_URL}/api${currentApi.endpoint}`,
+    );
+  }
   return (
     <>
-    <section className="api-explorer">
-      <div className="api-container">
-        <h2>Available APIs</h2>
-        {/* API Tab start from here */}
+      <section className="api-explorer">
+        <div className="api-container">
+          <h2>Available APIs</h2>
+          {/* API Tab start from here */}
 
-        <div className="api-tabs">
-          {Object.keys(apiConfig).map((api) => (
-            <button
-              key={api}
-              className={selectedApi === api ? "active-tab" : ""}
-              onClick={() =>{ 
-                  setSelectedApi(api)
+          <div className="api-tabs">
+            {Object.keys(apiConfig).map((api) => (
+              <button
+                key={api}
+                className={selectedApi === api ? "active-tab" : ""}
+                onClick={() => {
+                  setSelectedApi(api);
                   setSelectedFilters({});
-                  setGeneratedUrl(`${import.meta.env.VITE_API_URL}/api${apiConfig[api].endpoint}`);
-              }}
-            >
-              {apiConfig[api].label}
-            </button>
-          ))}
-        </div>
-        <div className="filter-panel">
-          <div className="filter-header">
-            <h3>Filters to apply on URL</h3>
-            <div className="filter-buttons">
-              <button className="reset-btn" onClick={handleResetFilters}>Reset Filters</button>
-
-              <button className="apply-btn" onClick={handleApplyFilters}>Apply Filters</button>
-            </div>
-          </div>
-          <div className="filter-grid">
-            {currentApi.filters.map((filter) => (
-              <div className="filter-item" key={filter.id}>
-                <label htmlFor={filter.id}>{filter.label}</label>
-
-                <select
-                  id={filter.id}
-                  value={selectedFilters[filter.id] || ""}
-                  onChange={handleFilterChange}
-                >
-                  <option value="">Select {filter.label}</option>
-
-                  {filter.options.map((option) => {
-                    const value=typeof option==="object" ? option.value : option
-                    const label=typeof option==="object" ? option.label: option
-                    return  (
-                      <option key={value} value={value}>{label}</option>
-
-                    )
-                  })}
-                </select>
-              </div>
+                  setGeneratedUrl(
+                    `${import.meta.env.VITE_API_URL}/api${apiConfig[api].endpoint}`,
+                  );
+                }}
+              >
+                {apiConfig[api].label}
+              </button>
             ))}
           </div>
-        </div>
+          <div className="filter-panel">
+            <div className="filter-header">
+              <h3>Filters to apply on URL</h3>
+              <div className="filter-buttons">
+                <button className="reset-btn" onClick={handleResetFilters}>
+                  Reset Filters
+                </button>
 
-        {/* URL Section */}
-        <div className="url-section">
-          <label>Final URL</label>
-          <div className="url-row">
-            <input
-              type="text"
-              value={generatedUrl}
-              readOnly
-            />
-            <button onClick={handleFetchData}>fetch data</button>
+                <button className="apply-btn" onClick={handleApplyFilters}>
+                  Apply Filters
+                </button>
+              </div>
+            </div>
+            <div className="filter-grid">
+              {currentApi.filters.map((filter) => (
+                <div className="filter-item" key={filter.id}>
+                  <label htmlFor={filter.id}>{filter.label}</label>
+
+                  <select
+                    id={filter.id}
+                    value={selectedFilters[filter.id] || ""}
+                    onChange={handleFilterChange}
+                  >
+                    <option value="">Select {filter.label}</option>
+
+                    {filter.options.map((option) => {
+                      const value =
+                        typeof option === "object" ? option.value : option;
+                      const label =
+                        typeof option === "object" ? option.label : option;
+                      return (
+                        <option key={value} value={value}>
+                          {label}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+              ))}
+            </div>
           </div>
+
+          {/* URL Section */}
+          <div className="url-section">
+            <label>Final URL</label>
+            <div className="url-row">
+              <input type="text" value={generatedUrl} readOnly />
+              <button onClick={handleFetchData}>fetch data</button>
+            </div>
+          </div>
+          {responseData && <Apimetadata metadata={apiMetadata} />}
+          {console.log(apiMetadata)}
+          {/* Response Section */}
+          <ResponseViewer data={responseData} loading={loading} error={error} />
         </div>
-        {/* Response Section */}
-        <ResponseViewer data={responseData} loading={loading} error={error}/>
-      </div>
-      {console.log(selectedFilters)}
-    </section>
-      <Codegenerator generatedUrl={generatedUrl} selectedApi={selectedApi}/>
-      </>
+        {console.log(selectedFilters)}
+      </section>
+      <Codegenerator generatedUrl={generatedUrl} selectedApi={selectedApi} />
+    </>
   );
 }
 
---
+++ frontend/src/components/ApiExplorer/ApiMetadata.jsx
@@ -0,0 +1,44 @@
+import "./ApiMetadata.css"
+
+function Apimetadata(props) {
+  const apiMetadata = props.metadata;
+
+  return (
+    <div className="api-metadata">
+
+      <section>
+        <h3>Request</h3>
+
+        <p>
+          <strong>Method:</strong> {apiMetadata.method}
+        </p>
+        <p>
+          <strong>Body:</strong> None
+        </p>
+        
+      </section>
+
+      <section>
+        <h3>Response</h3>
+
+        <p>
+          <strong>Status:</strong>{" "}
+          {apiMetadata.status} {apiMetadata.statusText}
+        </p>
+
+        <p>
+          <strong>Response Time:</strong>{" "}
+          {apiMetadata.responseTime} ms
+        </p>
+
+        <p>
+          <strong>Content Type:</strong>{" "}
+          {apiMetadata.contentType}
+        </p>
+      </section>
+
+    </div>
+  );
+}
+
+export default Apimetadata
